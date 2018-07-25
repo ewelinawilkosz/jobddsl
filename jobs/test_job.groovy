a6f8516a73c11917187ef8d1cab6cd22cae75179
Ewelina Wilkosz <ewelina.w@gmail.com>
2018-07-25 11:11:43 +0200

    loop test

--- jobs/test_job.groovy
+++ jobs/test_job.groovy
@@ -1,13 +1,17 @@
-job('test-job') {
-  description('this job does almost nothing, just a test')
-  logRotator(30, 15, -1, -1)
-  parameters {
-    stringParam('RELEASE', 'R01.02.03.04', 'Release number')
-  }
-  //label('some_node_label')
-  steps {
-    batchFile {
-      command('echo cos tam')
+def job_list = ['jobA', 'jobB', 'jobC']
+
+job_list.each { name ->
+  job(name) {
+    description('this job does almost nothing, just a test')
+    logRotator(30, 15, -1, -1)
+    parameters {
+      stringParam('RELEASE', 'R01.02.03.04', 'Release number')
+    }
+    //label('some_node_label')
+    steps {
+      batchFile {
+        command('echo cos tam')
+      }
     }
   }
 }
