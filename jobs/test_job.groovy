816b1060e0b641c6a2e2c8a4482802f296d2a06d
Ewelina Wilkosz <ewelina.w@gmail.com>
2018-07-25 13:23:11 +0200

    conditions

--- jobs/test_job.groovy
+++ jobs/test_job.groovy
@@ -1,10 +1,17 @@
 def job_list = ['releaseA': [['jobA', 'labelA'], ['jobB', 'labelB']],
                 'releaseB': [['jobC', 'labelC'], ['jobD', 'labelD'], ['jobE', 'labelE']]]
-
 // in each iteration x and y will contain an element from job_list 
 // e.g. in first iteration x = 'releaseA' and y = [['jobA', 'labelA'], ['jobB', 'labelB']]
+
+def conditional_line = ''
 job_list.each { x, y ->
   print y
+  if (x == "releaseA") {
+    conditional_line = "costam A\n"
+  } 
+  if (x == "releaseB") {
+    conditional_line = "costam B\n"
+  }
 // here we iterate through y, and in each iteration we take one element from y ([['jobA', 'labelA'], ['jobB', 'labelB']])
 // and call it 'z', so in the first iteration z = ['jobA', 'labelA']
   y.each { z ->
@@ -18,7 +25,9 @@ job_list.each { x, y ->
       //label('some_node_label')
       steps {
         batchFile {
-          command('cd c:\\CTT_AUTOMATIC\\bt\\test\\vb\\log__%RELEASE%_XYZ\\ ' + z[1])
+          command('cd c:\\CTT_AUTOMATIC\\bt\\test\\vb\\log__%RELEASE%_XYZ\\\n' +
+                  conditional_line +
+                  'cd c:\\costam\\bt\\test\\vb\')
         }
       }
     }
