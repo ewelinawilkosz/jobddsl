371c7512028d1ff9701b057d30dd4903c568da04
Ewelina Wilkosz <ewelina.w@gmail.com>
2018-07-25 12:11:58 +0200

    more params for each job

--- jobs/test_job.groovy
+++ jobs/test_job.groovy
@@ -1,9 +1,14 @@
-def job_list = ['jobA', 'jobB', 'jobC']
+def job_list = [['jobA', 'labelA'], 
+                ['jobB', 'labelB'], 
+                ['jobC', 'labelC']]
 
-job_list.each { name ->
-  job(name) {
+// in each iteration params will contain an element from job_list 
+// e.g. in first iteration params = ['jobA', 'labelA']
+job_list.each { params ->
+  job(params[0]) {
     description('this job does almost nothing, just a test')
     logRotator(30, 15, -1, -1)
+    label(params[1])
     parameters {
       stringParam('RELEASE', 'R01.02.03.04', 'Release number')
     }
